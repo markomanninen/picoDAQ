da5588c2e072192a154e16c8bc21634c73760700
Marko Manninen <elonmedia@gmail.com>
2021-04-19 09:34:00 +0300

    white space fixes
    fixing only some white spaces in this commit

--- picodaqa/picoConfig.py
+++ picodaqa/picoConfig.py
@@ -7,19 +7,19 @@ import numpy as np, time, sys
 class PSconfig(object):
   '''set PicoScope configuration'''
 
-  def __init__(self, confdict=None):
-    if confdict==None: confdict={}
+  def __init__(self, confdict = None):
+    if confdict == None: confdict = {}
 
 # set configuration parameters
 # -- PicoScope model
-    if "PSmodel" in confdict: 
+    if "PSmodel" in confdict:
       self.PSmodel = confdict["PSmodel"]
       if type(self.PSmodel) != type(''):
         self.PSmodel = str(self.PSmodel)
     else:
-      self.PSmodel = '2000a' 
+      self.PSmodel = '2000a'
 # -- channels to be used
-    if "picoChannels" in confdict: 
+    if "picoChannels" in confdict:
       self.picoChannels = confdict["picoChannels"]
     else:
       self.picoChannels = ['A', 'B'] # channels
@@ -32,11 +32,11 @@ class PSconfig(object):
     else:
       self.ChanRanges=[30E-3, 0.35]  # voltage range chan. A&B
 # -- signal timing
-  # note: picoscope.setSamplingInterval uses next larger value for 
+  # note: picoscope.setSamplingInterval uses next larger value for
     if "Nsamples" in confdict:
       self.Nsamples = confdict["Nsamples"]
     else:
-      self.Nsamples = 200  # number of samples to take 
+      self.Nsamples = 200  # number of samples to take
     if "sampleTime" in confdict:
       self.sampleTime = confdict["sampleTime"]
     else:
@@ -44,7 +44,7 @@ class PSconfig(object):
 
 # -- trigger configuration
     if "trgChan" in confdict:
-      self.trgChan = confdict["trgChan"]  
+      self.trgChan = confdict["trgChan"]
     else:
       self.trgChan = 'A'      # trigger channel,
     if "trgThr" in confdict:
@@ -61,25 +61,25 @@ class PSconfig(object):
     else:
       self.frqSG = 100E3
 #
-# get other parameters 
-    if "ChanModes" in confdict: 
+# get other parameters
+    if "ChanModes" in confdict:
       self.ChanModes = confdict['ChanModes']
     else:
       self.ChanModes = ['AC' for i in range(self.NChannels)]
-    if "ChanOffsets" in confdict: 
+    if "ChanOffsets" in confdict:
       self.ChanOffsets = confdict['ChanOffsets']
     else:
-      self.ChanOffsets= [0. for i in range(self.NChannels)]  
+      self.ChanOffsets= [0. for i in range(self.NChannels)]
        # voltage offsets   !!! not yet functional in driver
-    if "trgDelay" in confdict: 
+    if "trgDelay" in confdict:
       self.trgDelay=confdict["trgDelay"]
     else:
       self.trgDelay = 0        #
-    if "trgActive" in confdict: 
+    if "trgActive" in confdict:
       self.trgActive=confdict["trgActive"]
     else:
       self.trgActive = True   # no triggering if set to False
-    if "pretrig" in confdict: 
+    if "pretrig" in confdict:
       self.pretrig=confdict["pretrig"]
     else:
       self.pretrig=0.05      # fraction of samples before trigger
@@ -87,50 +87,50 @@ class PSconfig(object):
       self.pretrig = 0.
       print('  Pretrig sampling disabled on device ', self.PSmodel)
 
-    if "trgTO"  in confdict: 
-      self.trgTO=confdict["trgTO"] 
+    if "trgTO"  in confdict:
+      self.trgTO=confdict["trgTO"]
     else:
       self.trgTO=1000             #  and time-out
 # configuration of AWG
-    if "swpSG" in confdict: 
+    if "swpSG" in confdict:
       self.swpSG=confdict["swpSG"]
     else:
       self.swpSG = 'UpDown'
-    if "PkToPkSG" in confdict: 
+    if "PkToPkSG" in confdict:
       self.PkToPkSG = confdict["PkToPkSG"]
     else:
-      self.PkToPkSG = 0.4 
-    if "waveTypeSG" in confdict: 
+      self.PkToPkSG = 0.4
+    if "waveTypeSG" in confdict:
       self.waveTypeSG = confdict["waveTypeSG"]
     else:
       self.waveTypeSG = 'Sine'
-    if "stopFreqSG" in confdict: 
+    if "stopFreqSG" in confdict:
       self.stopFreqSG = confdict["stopFreqSG"]
     else:
       self.stopFreqSG = 9 * self.frqSG
-    if "dwellTimeSG" in confdict: 
+    if "dwellTimeSG" in confdict:
       self.dwellTimeSG = confdict["dwellTimeSG"]
     else:
-      if self.frqSG != 0:     
+      if self.frqSG != 0:
         self.dwellTimeSG = 10./self.frqSG
       else:
         self.dwellTimeSG = 0.
-    if "offsetVoltageSG" in confdict: 
-      self.offsetVoltageSG = confdict["offsetVoltageSG"] 
+    if "offsetVoltageSG" in confdict:
+      self.offsetVoltageSG = confdict["offsetVoltageSG"]
     else:
-      self.offsetVoltageSG = 0.  
-    if "verbose" in confdict: 
+      self.offsetVoltageSG = 0.
+    if "verbose" in confdict:
       self.verbose = confdict["verbose"]
     else:
-      self.verbose=1   # print (detailed) info if >0 
+      self.verbose=1   # print (detailed) info if >0
 
 # control printout, colors, ...
-    if "ChanColors" in confdict: 
+    if "ChanColors" in confdict:
       self.ChanColors=confdict["ChanColors"]
     else:
-      self.ChanColors = ['darkblue', 'darkslategrey', 'darkred', 'darkgreen']   
+      self.ChanColors = ['darkblue', 'darkslategrey', 'darkred', 'darkgreen']
 
-    if "mode" in confdict: 
+    if "mode" in confdict:
       self.mode = confdict["mode"] # "VMeter" "test"
 # - end PSconf.__init__()
 
@@ -144,8 +144,8 @@ class PSconfig(object):
       print(str(e))
       print('  - exiting')
       sys.exit(1)
-    try:      
-      exec('self.picoDevice = ps'+self.PSmodel+'.PS'+self.PSmodel+'()')  
+    try:
+      exec('self.picoDevice = ps'+self.PSmodel+'.PS'+self.PSmodel+'()')
     except Exception as e:
       print('!!! PSconfig:  Error initialising device')
       print(str(e))
@@ -155,9 +155,9 @@ class PSconfig(object):
     self.TSampling = 0.
     self.NSamples = 0.
     self.CRanges = [0., 0., 0., 0.]
-   
-    try: 
-      self.picoIni() # run initialisation routine for device  
+
+    try:
+      self.picoIni() # run initialisation routine for device
     except Exception as e:
       print("!!! PSconfig: Error configuring device")
       print(str(e))
@@ -212,8 +212,8 @@ class PSconfig(object):
 # 2) Channel Ranges
       CRanges=[]
       for i, Chan in enumerate(self.picoChannels):
-        CRanges.append(self.picoDevice.setChannel(Chan, self.ChanModes[i], 
-                   self.ChanRanges[i], VOffset=self.ChanOffsets[i], 
+        CRanges.append(self.picoDevice.setChannel(Chan, self.ChanModes[i],
+                   self.ChanRanges[i], VOffset=self.ChanOffsets[i],
                    enabled=True, BWLimited=False) )
         if verbose>0:
           print(prompt+"range channel %s: %.3gV (%.3gV)" \
@@ -222,7 +222,7 @@ class PSconfig(object):
           %(self.picoChannels[i], self.ChanOffsets[i]))
 # 3) enable trigger
     self.picoDevice.setSimpleTrigger(self.trgChan, self.trgThr, self.trgTyp,
-          self.trgDelay, self.trgTO, enabled=self.trgActive)    
+          self.trgDelay, self.trgTO, enabled=self.trgActive)
     if verbose>0:
       if self.trgActive:
         print(prompt+"trigger channel %s enabled: %.3gV %s" %\
@@ -230,11 +230,11 @@ class PSconfig(object):
       else:
         print(prompt+"picoIni: trigger inactive")
 
-# 4) enable Signal Generator 
+# 4) enable Signal Generator
     if self.frqSG !=0. :
-      self.picoDevice.setSigGenBuiltInSimple(frequency=self.frqSG, 
-         pkToPk=self.PkToPkSG, waveType=self.waveTypeSG, 
-         offsetVoltage=self.offsetVoltageSG, sweepType=self.swpSG, 
+      self.picoDevice.setSigGenBuiltInSimple(frequency=self.frqSG,
+         pkToPk=self.PkToPkSG, waveType=self.waveTypeSG,
+         offsetVoltage=self.offsetVoltageSG, sweepType=self.swpSG,
          dwellTime=self.dwellTimeSG, stopFreq=self.stopFreqSG)
       if verbose>0:
         print(prompt+"signal generator enabled: %.3gHz, +/-%.3g V %s"\
@@ -255,31 +255,31 @@ class PSconfig(object):
   def acquireDataBM(self, buffer):
     '''
     read data from device
-      this part is hardware (i.e. driver) specific code for PicoScope device,
-      interfaces to BufferMan.py 
+    this part is hardware (i.e. driver) specific code for PicoScope device,
+    interfaces to BufferMan.py
       Args:
         buffer: space to store data
 
       Returns:
         ttrg: time when device became ready
         tlife life time of device
-  '''
+    '''
     if self.pretrig != 0.:
-      self.picoDevice.runBlock(pretrig=self.pretrig) #
+      self.picoDevice.runBlock(pretrig = self.pretrig)
     else:
-      self.picoDevice.runBlock() #
-    ti=time.time()
+      self.picoDevice.runBlock()
+    ti = time.time()
     while not self.picoDevice.isReady():
       if not self.BM.ACTIVE.value: return None
       time.sleep(0.0001)
     # waiting time for occurence of trigger is counted as life time
-    ttrg=time.time()
+    ttrg = time.time()
     # account life time, w. appr. corr. for set-up time
     tlife = ttrg - ti - self.toverhead
-  # store raw data in global array 
+    # store raw data in global array
     for i, C in enumerate(self.picoChannels):
-      self.picoDevice.getDataRaw(C, self.NSamples, data=self.rawBuf[i])
-      self.picoDevice.rawToV(C, self.rawBuf[i], buffer[i], dtype=np.float32)
+      self.picoDevice.getDataRaw(C, self.NSamples, data = self.rawBuf[i])
+      self.picoDevice.rawToV(C, self.rawBuf[i], buffer[i], dtype = np.float32)
 # alternative:
      # self.picoDevice.getDataV(C, NSamples, dataV=VBuf[ibufw,i], dtype=np.float32)
     return ttrg, tlife
@@ -288,7 +288,7 @@ class PSconfig(object):
   def acquireData(self, buffer):
     '''
     read data from device
-      this part is hardware (i.e. driver) specific code for PicoScope device
+    this part is hardware (i.e. driver) specific code for PicoScope device
 
       Args:
         buffer: space to store data
@@ -296,22 +296,22 @@ class PSconfig(object):
       Returns:
         ttrg: time when device became ready
         tlife life time of device
-  '''
+    '''
     if self.pretrig !=0.:
-      self.picoDevice.runBlock(pretrig=self.pretrig)
-    else:  
-     self.picoDevice.runBlock() #
-    ti=time.time()
+      self.picoDevice.runBlock(pretrig = self.pretrig)
+    else:
+      self.picoDevice.runBlock()
+    ti = time.time()
     while not self.picoDevice.isReady():
       time.sleep(0.0001)
     # waiting time for occurence of trigger is counted as life time
-    ttrg=time.time()
+    ttrg = time.time()
     # account life time, w. appr. corr. for set-up time
     tlife = ttrg - ti - self.toverhead
-  # store raw data in global array 
+    # store raw data in global array
     for i, C in enumerate(self.picoChannels):
-      self.picoDevice.getDataRaw(C, self.NSamples, data=self.rawBuf[i])
-      self.picoDevice.rawToV(C, self.rawBuf[i], buffer[i], dtype=np.float32)
+      self.picoDevice.getDataRaw(C, self.NSamples, data = self.rawBuf[i])
+      self.picoDevice.rawToV(C, self.rawBuf[i], buffer[i], dtype = np.float32)
 # alternative:
      # self.picoDevice.getDataV(C, NSamples, dataV=VBuf[ibufw,i], dtype=np.float32)
     return ttrg, tlife
@@ -319,12 +319,10 @@ class PSconfig(object):
 
   def closeDevice(self):
     '''
-      Close down hardwre device
+    Close down hardware device
     '''
-    prompt = 4*' ' + 'PSconf: '
+    prompt = 4 * ' ' + 'PSconf: '
     if self.verbose: print(prompt + "closing connection to device")
     self.picoDevice.stop()
     self.picoDevice.close()
     time.sleep(0.5)
-
-
