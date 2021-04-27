e2837c02f3a514c4ae7c4cd4a769893fcdc19342
Marko Manninen <elonmedia@gmail.com>
2021-04-27 08:30:41 +0300

    future def to single line
    future def to single line

--- examples/runDataGraphs.py
+++ examples/runDataGraphs.py
@@ -8,8 +8,7 @@ displays data as effective voltage, history display and xy plot
 Usage: ./runDataGraphs.py [<Oscilloscpope_config>.yaml Interval]
 '''
 
-from __future__ import print_function, division, unicode_literals
-from __future__ import absolute_import
+from __future__ import print_function, division, unicode_literals, absolute_import
 
 import sys, time, yaml, numpy as np, threading, multiprocessing as mp
 
